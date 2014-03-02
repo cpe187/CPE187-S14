a914592263b1de724860b9419a6da0a06ffdbba1
gabe0 <g.hojman@icloud.com>
2014-03-01 16:44:42 -0800

    SPI Bus is now compatible with EEPROM
    The EEPROM requires that new data from MOSI line be valid on the POSEDGE
    rather than on NEGEDGE. This new revision is now compatible with the
    specification, at least in simulation. Synthesizing still brings errors.

--- spibus-verilog/old spibus.v/master.v
+++ spibus-verilog/old spibus.v/master.v
@@ -24,78 +24,84 @@ module master(
     input start,
 	 input reset,
 	 input miso,
-    output reg mosi,
-    output reg sclk,
-    output reg ss
+    output mosi,
+    output sclk,
+    output ss
     );
+	 
+	reg sclk, mosi, ss;
 	
 	reg [1:0] curr_state, next_state;
 	reg [7:0] buffer, transfer_reg;
 	
-	reg [4:0] cnt;
+	reg [3:0] cnt;
 	
 	initial cnt = 0;
 	
 	parameter IDLE = 3'b000, LOAD = 3'b001, TRANSFER = 3'b10, FINISH = 3'b11, UNLOAD = 3'b100;
 	
-	always@(posedge clk or negedge reset) //sequential posedge block
+	always@(posedge clk or negedge reset)
 	begin
 		sclk <= clk;
-		if(reset == 0) //if reset
+		if(reset == 0)
 		begin
-			transfer_reg <= 'b0;
-			buffer <= 'b0;
 			next_state <= IDLE;
-			mosi <= 1'b0;
-			ss <= 1'b1;
+			sclk = 1'b1;
+			mosi = 1'b0;
+			ss = 1'b1;
 		end
-		if(start == 1'b1) //when posedge clk && start == 1
-		begin
-			next_state <= LOAD;
-		end
-		else if(curr_state == IDLE) //accept new data into buffer when IDLE
-		begin
+		else
+		begin //only execute at the posedge of clock
+			curr_state <= next_state;
+			if(ss == 1'b0) 
+			begin
+				transfer_reg[7:0] <= transfer_reg[7:0] << 1;
+				transfer_reg[0] <= miso;
+			end
+			else
+			begin
 				buffer[7:0] <= buffer[7:0] << 1;
 				buffer[0] <= din;
+			end
 		end
 	end
 	
-	always@(negedge clk) //sequential negedge block
+	always@(negedge clk)
 	begin
-		sclk <= clk;
-		curr_state <= next_state;
-		if(ss == 1'b0) //transfer new data bit from MISO
+		sclk = clk;
+		if(start == 1'b1) //when negedge clk && start == 1
 		begin
-			transfer_reg[7:0] <= transfer_reg[7:0] << 1;
-			transfer_reg[0] <= miso;
+			next_state <= LOAD;
 		end
-		if(curr_state == IDLE) //accept new data into buffer when IDLE
+		else //if we're not done loading data, then accept new data into buffer
 		begin
 			buffer[7:0] <= buffer[7:0] << 1;
 			buffer[0] <= din;
 		end
 	end
 	
-	always@(curr_state or transfer_reg) //combinational block
+	always@(curr_state or transfer_reg)
 	begin
 		case(curr_state)
 			IDLE:
 			begin
-				ss = 1'b1;
+
 			end
+			
 			LOAD:
 			begin
 				transfer_reg[7:0] = buffer[7:0];
 				next_state = TRANSFER;
 			end
+			
 			TRANSFER:
 			begin
-				if(cnt < 16)
+				if(cnt < 8)
 				begin
 					ss = 1'b0;
 					mosi = transfer_reg[7];
 					cnt = cnt + 1;
-					if(cnt == 16)
+					if(cnt == 8)
 					begin
 						next_state = FINISH;
 					end
@@ -106,6 +112,7 @@ module master(
 			begin
 				ss = 1'b1;
 				cnt = 1'b0;
+				sclk = 1'b1;
 				next_state = UNLOAD;
 			end
 			
---
+++ spibus-verilog/top_tb.v
@@ -0,0 +1,101 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:15:38 02/11/2014
+// Design Name:   master
+// Module Name:   C:/Users/hojmang/Documents/cpe187/spibus/top_tb.v
+// Project Name:  spibus
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: master
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module top_tb;
+
+	// Inputs
+	reg clk;
+	reg din;
+	reg start;
+	reg reset;
+
+	// Outputs
+	wire mosi;
+	wire sclk;
+	wire ss;
+
+	// Instantiate the Unit Under Test (UUT)
+	master master1 (
+		.clk(clk), 
+		.din(din), 
+		.start(start),
+		.reset(reset),
+		.miso(miso),
+		.mosi(mosi),
+		.sclk(sclk), 
+		.ss(ss)
+	);
+	
+//	slave slave1 (
+//		.mosi(mosi),
+//		.sclk(sclk),
+//		.ss(ss),
+//		.miso(miso),
+//		.rdata(rdata)
+//	);
+
+	always #10 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		din = 0;
+		start = 0;
+		reset = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		reset = 0;
+		
+		#10;
+		reset = 1;
+		#30;
+		din = 1'b0;
+		#10;
+		din = 1'b0;
+		#10;
+		din = 1'b0;
+		#10;
+		din = 1'b0;
+		#10;
+		din = 1'b0;
+		#10;
+		din = 1'b1;
+		#10;
+		din = 1'b0;
+		#10;
+		din = 1'b1	;
+		start = 1;
+		#10;
+		start = 0;
+		#400;
+		start = 1;
+		#10;
+		start = 0;
+	end
+      
+endmodule
+
