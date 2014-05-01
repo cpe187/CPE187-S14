40620347bb498dc649c1b4503e70e16736568125
Arrch <logan.odell@gmail.com>
2014-05-01 14:38:01 -0700

    Created skeleton sketches for the main unit and wireless thermometer

---
+++ ClimateControlSystem/Main_Unit/Main_Unit.ino
@@ -0,0 +1,40 @@
+/* Project Climate Control Vent System - Main Unit
+ * CSUS - CPE 187 Spring 2014
+ *
+ * The Climate Control Vent System consists of wireless temperature sensors 
+ * and servo motors. Each room will have a temperature sensor and servo motor.
+ * The base unit receives sensor reading from the wireless sensor and makes
+ * decisons to turn on the servos and control the HVAC.
+ *
+ * Authors: Logan Odell, 
+ */
+
+/* ***************************************************
+ * INCLUDES
+ * Add any includes to this section
+ * ***************************************************/
+ 
+/* ***************************************************
+ * CONSTANTS
+ * Add any constants, defines or macros to this section
+ * ***************************************************/
+ 
+/* ***************************************************
+ * GLOBALS
+ * Add any global variables to this section
+ * ***************************************************/
+ 
+ /* Setup function is run once on startup. Use this function to initialize 
+ * any variables, pin modes, libraries or make other one-time calls */
+void setup()
+{
+  
+}
+
+/* Loop function is repeated indefinitely. Any code that should continuously
+ * be executed, should be place in this function. Avoid using delay calls by
+ * using the concepts demonstrated in the BlinkWithoutDelay() example */
+void loop()
+{
+  
+}
